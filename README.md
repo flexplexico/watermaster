16482f11d8e8d25f84dc36fcd884006116b8a92e
David Petersen <dave@the-petersens.org>
2013-04-07 12:00:21 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+== Welcome to WaterMaster
+
+WaterMaster is a really cool project.  It will be used to control residential sprinkler systems.  It's unique water scheduling algorithmn takes the guesswork out of watering your lawn. You will only need to enter the number of zones and some basic information about each zone, your location and the date range that you need to irrigate. 
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watermaster::Application.config.secret_token = 'b0515a4a4e7ad997b5a7e6b0d67ac466381a15e85f38731f5396488d227c0d53b20782ea4b0021aac4636ff58e5259b48c7fe9fcfdb26f39def94a2586653fc5'
